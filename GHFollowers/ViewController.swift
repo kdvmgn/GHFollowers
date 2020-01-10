9acf79a67721699337515ef660cb94aa13e74b3a
Kuleshov Dmitry Victorovich <dkuleshov@stream.ru>
2020-01-10 13:33:50 +0300

    Initial Commit

---
+++ GHFollowers/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  GHFollowers
+//
+//  Created by Кулешов Дмитрий Викторович on 10.01.2020.
+//  Copyright © 2020 Dmitry Kuleshov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
