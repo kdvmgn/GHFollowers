7c3ecb0bb876c0d123fc539e9ef51f2e7b15cdd3
Kuleshov Dmitry Victorovich <dkuleshov@stream.ru>
2020-01-10 17:17:52 +0300

    [tabbar-navigation] configure search screen

--- GHFollowers/SearchViewController.swift
+++ GHFollowers/SearchViewController.swift
@@ -9,11 +9,64 @@
 import UIKit
 
 class SearchViewController: UIViewController {
+    
+    // MARK: - Properties
+    
+    let logoImage = UIImageView()
+    
+    let userNameTextField = GHTextField()
+    
+    let searchActionButton = GHButton(backgroundColor: .systemGreen, title: "Search Followers")
 
     // MARK: - Life cycle
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        view.backgroundColor = .systemBlue
+        view.backgroundColor = .systemBackground
+        configureLogoImage()
+        configureTextField()
+        configureSearchActionButton()
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        navigationController?.isNavigationBarHidden = true
+    }
+    
+    // MARK: - Private functions
+    
+    private func configureLogoImage() {
+        view.addSubview(logoImage)
+        logoImage.translatesAutoresizingMaskIntoConstraints = false
+        logoImage.image = #imageLiteral(resourceName: "gh-logo")
+        
+        NSLayoutConstraint.activate([
+            logoImage.widthAnchor.constraint(equalToConstant: 200),
+            logoImage.heightAnchor.constraint(equalToConstant: 200),
+            logoImage.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 80),
+            logoImage.centerXAnchor.constraint(equalTo: view.centerXAnchor)
+        ])
+    }
+    
+    private func configureTextField() {
+        view.addSubview(userNameTextField)
+        
+        NSLayoutConstraint.activate([
+            userNameTextField.heightAnchor.constraint(equalToConstant: 50),
+            userNameTextField.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 50),
+            userNameTextField.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -50),
+            userNameTextField.topAnchor.constraint(equalTo: logoImage.bottomAnchor, constant: 48)
+        ])
+    }
+    
+    private func configureSearchActionButton() {
+        view.addSubview(searchActionButton)
+        
+        NSLayoutConstraint.activate([
+            searchActionButton.heightAnchor.constraint(equalToConstant: 50),
+            searchActionButton.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 50),
+            searchActionButton.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -50),
+            searchActionButton.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor, constant: -50)
+        ])
     }
 }
