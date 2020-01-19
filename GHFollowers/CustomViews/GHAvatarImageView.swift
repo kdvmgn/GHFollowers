11538e099ec244eec34f3f99567397fc0ebc91bc
Dmitry Kuleshov <kdv.mgn@gmail.com>
2020-01-19 19:38:50 +0300

    [GHFollowers-network-layer] add follower collection view cell

---
+++ GHFollowers/CustomViews/GHAvatarImageView.swift
@@ -0,0 +1,31 @@
+//
+//  GHAvatarImageView.swift
+//  GHFollowers
+//
+//  Created by Дмитрий Кулешов on 19.01.2020.
+//  Copyright © 2020 Dmitry Kuleshov. All rights reserved.
+//
+
+import UIKit
+
+class GHAvatarImageView: UIImageView {
+
+    // MARK: - Initializer
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        configure()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    // MARK: - Private functions
+    
+    private func configure() {
+        layer.cornerRadius = 10
+        clipsToBounds = true
+        translatesAutoresizingMaskIntoConstraints = false
+    }
+}
