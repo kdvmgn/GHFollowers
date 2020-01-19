6f579fc2d6ce4cd0773899cc18a26b99778ae231
Dmitry Kuleshov <kdv.mgn@gmail.com>
2020-01-19 21:55:18 +0300

    [GHFollowers-network-layer] followers list custom flow layout

--- GHFollowers/Screens/FolloverListViewController.swift
+++ GHFollowers/Screens/FolloverListViewController.swift
@@ -50,9 +50,23 @@ class FolloverListViewController: UIViewController {
     }
     
     private func configureCollectionView() {
-        collectionView = UICollectionView(frame: view.bounds, collectionViewLayout: UICollectionViewFlowLayout())
+        collectionView = UICollectionView(frame: view.bounds,
+                                          collectionViewLayout: createThreeColumnsLayout())
         view.addSubview(collectionView)
         collectionView.backgroundColor = .systemPink
+    }
+    
+    private func createThreeColumnsLayout() -> UICollectionViewFlowLayout {
+        let width: CGFloat = view.bounds.width
+        let padding: CGFloat = 12.0
+        let minimumItemSpacing: CGFloat = 10.0
+        let availableWidth: CGFloat = width - (2 * padding) - (2 * minimumItemSpacing)
+        let itemWidth: CGFloat = availableWidth / 3
+        
+        let flowLayout = UICollectionViewFlowLayout()
+        flowLayout.sectionInset = UIEdgeInsets(top: padding, left: padding, bottom: padding, right: padding)
+        flowLayout.itemSize = CGSize(width: itemWidth, height: itemWidth + 40)
+        return flowLayout
     }
     
     private func fetchFollowers() {
